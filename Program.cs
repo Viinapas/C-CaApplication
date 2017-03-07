d739fdb3f2baa0841d59a86785f62cb2c41a91a2
ekodi-repo <pasi.viinanen@gmail.com>
2017-03-07 07:09:09 +0200

    C# negative or positive number

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CaNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PV
+            // Write number. Input example 5 Output "Number 5 is positive"
+
+            float num = 0;
+            String keyword ;
+
+            Console.WriteLine(" Give Number");
+            keyword=Console.ReadLine();
+            num = float.Parse(keyword);
+
+            if (num >0)
+            {
+              Console.WriteLine( "Number"+" "+  num + " is positive");
+                Console.ReadLine();
+                return;
+
+            
+            }
+
+            if (num >=0)
+            {
+                Console.WriteLine( " Number" +" "+num + " is ZERO and it is positive");
+                Console.ReadLine();
+                return;
+
+            }
+
+
+
+            if (num < 0)
+            {
+                Console.WriteLine("Number" + " " + num + " is negative");
+                Console.ReadLine();
+                return;
+
+
+            }
+
+
+
+
+        }
+    }
+}
